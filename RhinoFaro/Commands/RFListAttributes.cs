5ca0c312db663adefd86990683589e9635470acc
Tom Svilans <tom.svilans@gmail.com>
2019-12-12 10:51:08 +0100

    Added ListAttributes command

---
+++ RhinoFaro/Commands/RFListAttributes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace RhinoFaro
+{
+    [System.Runtime.InteropServices.Guid("ec64b091-ed22-48aa-b72c-8deb811d9236")]
+    public class RFListAttributes : Command
+    {
+        static RFListAttributes _instance;
+        public RFListAttributes()
+        {
+            _instance = this;
+        }
+
+        public static RFListAttributes Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "RFListAttributes"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RFContext.ListAllAttributes();
+            return Result.Success;
+        }
+    }
+}
